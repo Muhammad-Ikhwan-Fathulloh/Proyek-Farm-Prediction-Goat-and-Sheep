2ce0dda1fc5f8e0a01df8a788322507ae48dfe1f
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2024-06-01 09:25:48 +0700

    Update Code Prediction

--- FullCode/FullCode.ino
+++ FullCode/FullCode.ino
@@ -45,7 +45,7 @@ int beatAvg;
 #include <DallasTemperature.h>
 
 // GPIO where the DS18B20 is connected to
-const int oneWireBus = 4;     
+const int oneWireBus = 2;     
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(oneWireBus);
@@ -183,52 +183,50 @@ void breathSensor(){
   delay(50);
 }
 
-void HeartRateSensor(){
+void HeartRateSensor() {
   long irValue = particleSensor.getIR();
 
-  if (checkForBeat(irValue) == true)
-  {
+  if (checkForBeat(irValue)) {
     //We sensed a beat!
     long delta = millis() - lastBeat;
     lastBeat = millis();
 
     beatsPerMinute = 60 / (delta / 1000.0);
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
+    if (beatsPerMinute < 255 && beatsPerMinute > 20) {
       rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
       rateSpot %= RATE_SIZE; //Wrap variable
 
       //Take average of readings
       beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
+      for (byte x = 0; x < RATE_SIZE; x++)
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
+
+    Serial.print("IR=");
+    Serial.print(irValue);
+    Serial.print(", BPM=");
+    Serial.print(beatsPerMinute);
+    Serial.print(", Avg BPM=");
+    Serial.print(beatAvg);
+
+    heartRate = beatsPerMinute;
+
+    //LCD Display
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Detak Jantung");
+    lcd.setCursor(0, 1);
+    lcd.print(String(beatsPerMinute) + "per Menit");
+
+    client.publish("158928/farm/heart_sensor", String(beatsPerMinute));
+
+    if (irValue < 50000)
+      Serial.print(" No finger?");
+
+    Serial.println();
   }
-
-  Serial.print("IR=");
-  Serial.print(irValue);
-  Serial.print(", BPM=");
-  Serial.print(beatsPerMinute);
-  Serial.print(", Avg BPM=");
-  Serial.print(beatAvg);
-
-  heartRate = beatsPerMinute;
-
-  //LCD Display
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Detak Jantung");
-  lcd.setCursor(0,1);
-  lcd.print(String(beatsPerMinute) + "per Menit");
-  
-  client.publish("158928/farm/heart_sensor", String(beatsPerMinute));
-  
-  if (irValue < 50000)
-    Serial.print(" No finger?");
-
-  Serial.println();
 }
 
 void TemperatureSensor(){
